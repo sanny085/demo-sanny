3fa7abace23dfa5dd87fb622dd0f455ce560f37c
sanny085 <sannykumar085@gmail.com>
2024-06-19 16:16:25 +0530

    first commit, displayed the four images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Cricket from "./bat_ball.jpg";
+import CricketBat from "./assets/cricket-bat.webp";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="react-icon" />
+        <img src={Cricket} className="App-logo" alt="small bat ball" />
+        <img src={CricketBat} className="App-logo" alt="bat pic" />
+        <img src="/header/dhoni.jpeg" className="App-logo" alt="dhoni pic" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
